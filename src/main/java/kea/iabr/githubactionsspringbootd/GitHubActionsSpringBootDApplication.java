e802431fc0e16a046a1a60202fa26f22bbc41fb6
GitHubBruger <ianb@kea.dk>
2024-02-27 13:27:18 +0100

    Initial commit

---
+++ src/main/java/kea/iabr/githubactionsspringbootd/GitHubActionsSpringBootDApplication.java
@@ -0,0 +1,13 @@
+package kea.iabr.githubactionsspringbootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsSpringBootDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsSpringBootDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kea/iabr/githubactionsspringbootd/GitHubActionsSpringBootDApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.iabr.githubactionsspringbootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsSpringBootDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
